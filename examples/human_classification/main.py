f69278a695d86e63b391f462c815cda7bb623eb7
Shuaih7 <shuaih7@gmail.com>
2020-10-05 22:56:20 +0800

    visualdl added

--- examples/human_classification/main.py
+++ examples/human_classification/main.py
@@ -1,6 +1,7 @@
 import os, paddle
 import numpy as np
 from paddle import fluid
+from visualdl import LogWriter
 from model import ResNet
 from data import train_generator, valid_generator
 
@@ -47,21 +48,47 @@ train_reader = paddle.batch(train_generator, batch_size=batch_size)
 test_reader  = paddle.batch(valid_generator, batch_size=batch_size)
 feeder=fluid.DataFeeder(place=place,feed_list=[image,label])
 
+# Create the logger
+log_path = r"E:\Projects\Engine_Inspection\VGG16CAM\ResNet50\log"
+log_writer = LogWriter(log_path, sync_cycle=5)
+# 获取训练全部参数
+params_name = fluid.default_startup_program().global_block().all_parameters()[0].name
+
+"""
+# This way will trigger the AttributeError: 'LogWriter' object has no attribute 'mode'
+with log_writer.mode('train') as writer:
+    train_cost_writer = writer.scalar('cost')
+    train_acc_writer = writer.scalar('accuracy')
+    histogram = writer.histogram('histogram', num_buckets=50)
+
+with log_writer.mode('test') as writer:
+    test_cost_writer = writer.scalar('cost')
+    test_acc_writer = writer.scalar('accuracy')
+"""
+
 ### References for future use: https://blog.csdn.net/qq_41427568/article/details/87735085
 ### Loading parameters: fluid.io.load_params(executor=exe,dirname=save_path) or fluid.io.load_persistables()
 ### Saving parameters:  fluid.io.save_params()
 ### Saving model for inference: fluid.io.save_inference_model(save_path,feeded_var_names=[image.name],target_vars=[model],executor=exe)
+### For LogWriter information please refer to: https://blog.csdn.net/hua111hua/article/details/89422661
 
 # Training
-epoch_num = 3
+epoch_num = 10
 report_freq = 10
 base_acc = 0.0
 model_save_path = r"E:\Projects\Engine_Inspection\VGG16CAM\ResNet50\model"
 var_save_path   = r"E:\Projects\Engine_Inspection\VGG16CAM\ResNet50\variable"
 
+train_step, test_step = 0, 0
 for epoch_id in range(epoch_num):
     for batch_id, data in enumerate(train_reader()):
-        train_cost,train_acc=exe.run(program=fluid.default_main_program(),feed=feeder.feed(data),fetch_list=[avg_cost,batch_acc])
+        train_cost, train_acc, params = exe.run(program=fluid.default_main_program(),feed=feeder.feed(data),fetch_list=[avg_cost,batch_acc,params_name])
+        
+        # Write the training data into the LogWriter
+        log_writer.add_scalar("train_cost", train_cost[0], train_step)
+        log_writer.add_scalar("train_acc", train_acc[0], train_step)
+        log_writer.add_histogram("histogram", params.flatten(), train_step, buckets=50)
+        train_step += 1
         
         # Printing the result every 100 batch
         if batch_id % report_freq == 0:
@@ -74,6 +101,12 @@ for epoch_id in range(epoch_num):
         test_cost,test_acc=exe.run(program=test_program,feed=feeder.feed(data),fetch_list=[avg_cost,batch_acc])
         test_accs.append(test_acc[0])
         test_costs.append(test_cost[0])
+        
+        # Write the training data into the LogWriter
+        log_writer.add_scalar("test_cost", test_cost[0], test_step)
+        log_writer.add_scalar("test_acc", test_acc[0], test_step)
+        test_step += 1
+        
     # Get the avergaed valiue
     test_cost=(sum(test_costs)/len(test_costs))
     test_acc=(sum(test_accs)/len(test_accs))
@@ -82,9 +115,8 @@ for epoch_id in range(epoch_num):
     
     # Saving the inference model
     if test_acc > base_acc: 
-        fluid.io.save(fluid.default_main_program(), model_save_path)
-        #fluid.io.save_inference_model(model_save_path,feeded_var_names=[image.name],target_vars=[predict],executor=exe)
-        fluid.io.save_params(executor=exe,dirname=os.path.join(var_save_path, str(epoch_id)))
+        fluid.io.save(fluid.default_main_program(), os.path.join(model_save_path, str(epoch_id)))
+        fluid.io.save_params(executor=exe,dirname=var_save_path)
         base_acc = test_acc # Update the baseline accuracy
         print("Model and variables are saved.")
     
--- examples/human_classification/test.py
+++ examples/human_classification/test.py
@@ -1,9 +1,25 @@
-import numpy as np
+# coding=utf-8
+from visualdl import LogWriter
 
-a = np.zeros((4,4), dtype=np.uint8)
-b = np.ones((4,4), dtype=np.uint8)
-c = np.ones((4,4), dtype=np.uint8)*2
+# 创建 LogWriter 对象
+log_writer = LogWriter(".", sync_cycle=20)
 
-d = np.stack((a,b,c), axis=0)
-print(d.shape)
-print(d)
+# 创建 scalar 组件，模式为 train
+with log_writer.mode("train") as logger:
+    train_acc = logger.scalar("acc")
+    train_loss = logger.scalar("loss")
+
+# 创建 scalar 组件，模式设为 test， tag 设为 acc
+with log_writer.mode("test") as logger:
+    test_acc = logger.scalar("acc")
+
+value = [i/1000.0 for i in range(1000)]
+for step in range(1000):
+    # 向名称为 acc 的图中添加模式为train的数据
+    train_acc.add_record(step, value[step])
+
+    # 向名称为 loss 的图中添加模式为train的数据
+    train_loss.add_record(step, 1 / (value[step] + 1))
+    
+    # 向名称为 acc 的图中添加模式为test的数据
+    test_acc.add_record(step, 1 - value[step])
