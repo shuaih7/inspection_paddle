cd9948d486b8a7bfd4e3697674a97f5d320a26e8
Shuaih7 <shuaih7@gmail.com>
2020-10-11 23:27:30 +0800

    evaluation

--- examples/fabric_defect_detection/data/image.py
+++ examples/fabric_defect_detection/data/image.py
@@ -2,10 +2,13 @@ import os, cv2
 import numpy as np
 
 
-def image_crop_generator(img_file, size=224, return_dim=True):
+def image_crop_generator(img_file, size=224, scale="down", return_dim=True):
     image = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
     h, w = image.shape[:2]
     hdim, wdim = int(h/size), int(w/size)
+    if scale == "up": 
+        if h % size > 0: hdim += 1
+        if w % size > 0: wdim += 1
     image = cv2.resize(image, (wdim*size,hdim*size), cv2.INTER_LINEAR)
     
     images = []
@@ -13,7 +16,24 @@ def image_crop_generator(img_file, size=224, return_dim=True):
         for i in range(wdim): images.append(image[j*size:(j+1)*size,i*size:(i+1)*size])
         
     images = np.array(images, dtype=np.float32) / 255.0
-    images = np.expand_dims(np.squeeze(images),1)
+    if len(images) == 1: images = np.expand_dims(np.expand_dims(np.squeeze(images),0),0)
+    else: images = np.expand_dims(np.squeeze(images),1)
     
     if return_dim: return images, [hdim, wdim]
     else: return images
+    
+    
+def mark_image(img_file, matrix, size=224, color=(255,0,0), line_width=5, offset=5):
+    image = cv2.imread(img_file, cv2.IMREAD_COLOR)
+    hdim, wdim = matrix.shape
+    image = cv2.resize(image, (wdim*size,hdim*size), cv2.INTER_LINEAR)
+    
+    for j in range(hdim):
+        for i in range(wdim): 
+            x1, y1 = i*size+offset, j*size+offset
+            x2, y2 = (i+1)*size-offset, (j+1)*size-offset
+            if matrix[j,i] > 0: image = cv2.rectangle(image, (x1, y1), (x2, y2), color, line_width)
+    return image
+    
+    
+    
--- examples/fabric_defect_detection/eval.py
+++ examples/fabric_defect_detection/eval.py
@@ -1,24 +1,32 @@
 import cv2, os
 import paddle.fluid as fluid
 import numpy as np
-from data.image import image_crop_generator
+from data.image import image_crop_generator, mark_image
+from matplotlib import pyplot as plt
 
-
-model_path = r"E:\Projects\Fabric_Defect_Detection\model_proto\saved_model"
-image_path = r"C:\Users\shuai\Documents\GitHub\inspection_paddle\examples\fabric_defect_detection\data\sample.png"
 
 use_cuda = True
+model_path = r"E:\Projects\Fabric_Defect_Detection\model_proto\saved_model"
 place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
 exe = fluid.Executor(place)
 exe.run(fluid.default_startup_program())
-
 [infer_program, feed_var_names, target_vars] = (fluid.io.load_inference_model(dirname=model_path, executor=exe))
 
-img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-img = img[:224,:224]
-img = np.array(img, dtype=np.float32) / 255.0
-img = np.expand_dims(np.expand_dims(img, 0), 0)
 
-result = exe.run(program=infer_program, feed={feed_var_names[0] :img}, fetch_list=target_vars)
-max_id = np.argmax(result[0][0])
-print(max_id, result[0][0][max_id])
+def evaluate(img_file, scale="up"):
+    images, dims = image_crop_generator(img_file, scale="up")
+    result = exe.run(program=infer_program, feed={feed_var_names[0] :images}, fetch_list=target_vars)
+    print(result[0])
+    
+    result = np.argmax(result[0], axis=1)
+    matrix = np.reshape(result, (dims[0], dims[1]))
+    res_img = mark_image(img_file, matrix, size=224)
+    plt.imshow(res_img)
+    plt.show(), plt.title("Prediction Result")
+
+   
+if __name__ == "__main__":
+    img_file = r"E:\Projects\Fabric_Defect_Detection\model_proto\dataset\random_valid_pos\1_51.png" 
+    evaluate(img_file)
+    
+    
