790a2bc9f558bc89183b553de273cff6766faa62
Shuaih7 <shuaih7@gmail.com>
2021-01-10 15:19:56 +0800

    test: label test for detection

--- projects/Part_Number/pytest.py
+++ projects/Part_Number/pytest.py
@@ -1,8 +1,21 @@
 import os
 import sys
+import random
 import numpy as np
 
-A = [2,3,4,5,6]
-B = np.array(A) // 2
 
-print(B)
+def search_labels(label_file):
+    data_lines = []
+    
+    with open(label_file, "rb") as f:
+        lines = f.readlines()
+        lines = random.sample(lines, 1)
+        print(lines)
+        data_lines.extend(lines)
+    return data_lines
+    
+
+if __name__ == "__main__":
+    label_file = r"E:\Projects\Part_Number\baidu\icdar2015\text_localization\test_icdar2015_label.txt"
+    data_lines = search_labels(label_file)
+    #print(data_lines)
--- projects/Part_Number/utils.py
+++ projects/Part_Number/utils.py
@@ -8,5 +8,6 @@ def draw_polylines(image, polylines, isClosed=True, color=(0,255,0), thickness=3
     polylines = np.array(polylines, dtype=np.int32)#.reshape((-1,1,2))
     for line in polylines:
         line = line.reshape((-1,1,2))
-        image = cv2.polylines(image, line, isClosed=isClosed, color=color, thickness=thickness)
+        image = cv2.polylines(image, [line], isClosed=isClosed, color=color, thickness=thickness)
     return image   
+    
